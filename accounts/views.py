18f8e0a7395652a056b749f8fbafbe30759b4fac
Deepak Singh <deepak16032@iiitd.ac.in>
2019-10-27 01:46:18 +0530

    second

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm 
+#from django.contrib.auth.forms import UserCreationForm 
 from django.urls import reverse_lazy 
 from django.views import generic
 from django.contrib.auth import get_user_model
-
+from .forms import CustomUserCreationForm
 class SignUpView(generic.CreateView): 
-    form_class = UserCreationForm 
+    form_class =  CustomUserCreationForm
     success_url = reverse_lazy('login') 
     template_name = 'signup.html'
-User = get_user_model()
+#User = get_user_model()
 
 # Create your views here.
--- fcs_project/settings.py
+++ fcs_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'blog',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
